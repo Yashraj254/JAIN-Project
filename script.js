e44f1bed1f43da13e5d4770363448bac41d35c1c
Yashraj254 <yashr7651@gmail.com>
2023-10-27 15:54:59 +0530

    increase the time interval for next click on nav bar icon

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-	$('#nav-icon').click(function(){
-		$(this).toggleClass('open');
-	});
-});
+    var canClick = true; // A flag to control whether the button can be clicked
+  
+    $('#nav-icon').click(function(){
+      if (canClick) {
+        $(this).toggleClass('open');
+        canClick = false; // Disable further clicks
+  
+        // Enable the button after 350ms 
+        setTimeout(function() {
+          canClick = true;
+        }, 350);
+      }
+    });
+  });
+  
