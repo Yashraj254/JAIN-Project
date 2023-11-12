c461bd12ff308b56c140e13e8a65f8bd0acc1741
Yashraj254 <yashr7651@gmail.com>
2023-11-12 19:45:50 +0530

    aligned login and register buttons to end

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     var canClick = true; // A flag to control whether the button can be clicked
   
-    $('#nav-icon').click(function(){
+    $('.navbar-toggler').click(function(){
       if (canClick) {
-        $(this).toggleClass('open');
+        $('#nav-icon').toggleClass('open');
         canClick = false; // Disable further clicks
   
         // Enable the button after 350ms 
