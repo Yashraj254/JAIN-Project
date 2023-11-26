1fd37ad8ec5b03e671167cb35e2078a671872f28
Yashraj Singh Jadon <yashr7651@gmail.com>
2023-11-26 14:54:31 +0530

    centered the logo in booking section

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ navLinks.forEach((link) => {
   });
 });
 
-//Booking Form Validation
+// Booking Form Validation
 
 (() => {
   "use strict";
@@ -66,7 +66,7 @@ navLinks.forEach((link) => {
 $("#startDateValidation")[0].min = new Date().toISOString().split("T")[0];
 
 $("#endDateValidation")[0].min = new Date(
-  new Date().setDate(new Date().getDate() + 2)
+  new Date().setDate(new Date().getDate() + 1)
 )
   .toISOString()
   .split("T")[0];
@@ -77,29 +77,34 @@ function setDestinationValue(newValue) {
   $("#destinationValidation").val(newValue);
 }
 
+// Scroll to exact package when clicking on a package item from navbar
+
 $('a[href*="#"]')
   .not('[href="#"]')
   .not('[href="#0"]')
   // Remove links that don't actually link to anything
-  .click(function(event) {
+  .click(function (event) {
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
       location.hostname == this.hostname
     ) {
       // Figure out element to scroll to
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       // Does a scroll target exist?
       if (target.length) {
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
 
-        var scrollTop = target.offset().top - $('.sticky-top').height();
+        var scrollTop = target.offset().top - $(".sticky-top").height();
 
-        $('html, body').animate({
-          scrollTop: scrollTop
-        }, 1000)
+        $("html, body").animate(
+          {
+            scrollTop: scrollTop,
+          },
+          500
+        );
       }
     }
-});
+  });
